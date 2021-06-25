c84c9dc7ac6b0dae56449837fc4ac4485c3f853c
rajonahmed749 <raazon749@gmail.com>
2021-06-26 00:47:07 +0600

    order route created

--- index.js
+++ index.js
@@ -24,6 +24,10 @@ app.get('/', (req, res) => {
 
 client.connect(err => {
   const serviceCollection = client.db("redMountain").collection("services");
+  const orderCollection = client.db("redMountain").collection("orders");
+  
+  console.log(err);
+
 
   // get all services
   app.get('/allServices', (req, res) => {
@@ -41,6 +45,38 @@ client.connect(err => {
       res.send(result.insertedCount > 0)
     })
   });
+
+
+  // checkout services
+    app.get("/service/:id", (req, res) => {    
+    serviceCollection.find({_id: ObjectId(req.params.id)})
+    .toArray((err, documents) =>{
+      res.send(documents[0]);
+      // console.log("this is documents",documents);
+      })
+  })
+
+  // add orders
+  app.post('/addOrder', (req, res) => {
+    const newService = req.body;    
+    orderCollection.insertOne(newService)
+    .then(result => {
+      res.send(result.insertedCount > 0)
+    })
+  });
+
+
+
+// get user specific order
+    app.get('/getOrders/:id', (req, res) => {
+      const email = req.params.id;
+      console.log(email)
+      orderCollection.find({ user: email })
+        .toArray((err, orders) => {
+          res.send(orders);
+          // res.send(orders.length > 0);
+        })
+  })
 
   
 
