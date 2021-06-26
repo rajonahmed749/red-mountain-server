c73f0d4b0e8abeb15e559805bb70859031de1fec
rajonahmed749 <raazon749@gmail.com>
2021-06-26 18:06:53 +0600

    checkout page and route created

--- index.js
+++ index.js
@@ -16,69 +16,59 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.get('/', (req, res) => {
-  res.send("This is first server after a long time")
+  res.send("This is Red Mountain Kids Server")
   console.log("database connected")
 })
-
 
 
 client.connect(err => {
   const serviceCollection = client.db("redMountain").collection("services");
   const orderCollection = client.db("redMountain").collection("orders");
-  
+
   console.log(err);
-
-
-  // get all services
-  app.get('/allServices', (req, res) => {
-    serviceCollection.find({})
-    .toArray((err, documents) =>{
-      res.send(documents)
+    // All services
+    app.get('/allServices', (req, res) => {
+      serviceCollection.find({})
+        .toArray((err, documents) => {
+          res.send(documents)
+        })
     })
-  })
 
-  // add service one by one
-  app.post('/addService', (req, res) => {
-    const newService = req.body;    
-    serviceCollection.insertOne(newService)
-    .then(result => {
-      res.send(result.insertedCount > 0)
+    // add one service
+    app.post('/addService', (req, res) => {
+      const newService = req.body;
+      serviceCollection.insertOne(newService)
+        .then(result => {
+          res.send(result.insertedCount > 0)
+        })
+    });
+
+    // checkout services
+    app.get("/service/:id", (req, res) => {
+      serviceCollection.find({ _id: ObjectId(req.params.id) })
+        .toArray((err, documents) => {
+          res.send(documents[0]);
+        })
     })
-  });
 
+    // place an Order
+    app.post('/addOrder', (req, res) => {
+      const newService = req.body;
+      orderCollection.insertOne(newService)
+        .then(result => {
+          res.send(result.insertedCount > 0)
+        })
+    });
 
-  // checkout services
-    app.get("/service/:id", (req, res) => {    
-    serviceCollection.find({_id: ObjectId(req.params.id)})
-    .toArray((err, documents) =>{
-      res.send(documents[0]);
-      // console.log("this is documents",documents);
-      })
-  })
-
-  // add orders
-  app.post('/addOrder', (req, res) => {
-    const newService = req.body;    
-    orderCollection.insertOne(newService)
-    .then(result => {
-      res.send(result.insertedCount > 0)
-    })
-  });
-
-
-
-// get user specific order
+    // get user specific order
     app.get('/getOrders/:id', (req, res) => {
       const email = req.params.id;
-      console.log(email)
       orderCollection.find({ user: email })
         .toArray((err, orders) => {
           res.send(orders);
-          // res.send(orders.length > 0);
         })
-  })
+    })
 
-  
 
 
 });
